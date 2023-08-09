9d270368fe4331229c19578b95fc0a2455e268a3
Cesar <cesar.alcoholado@hotmail.com>
2023-08-09 11:46:25 -0300

    Created basic unprotected routes for internal use

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 
+function App() {
   return (
     <>
-    <h2>Historial</h2>
+      <Routes>
+        //* Rutas para uso interno *//
+        <NavBar/>
+        <Route path="/" element={<Login/>}/>
+        //! Rutas protegidas //!
+        {/* <Route element={<ProtectedRoutes />}> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/config" element={<Config/>} />
+        {/* </Route> */}
+        //! Rutas protegidas //!
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
