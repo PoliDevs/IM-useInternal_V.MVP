87111f5cc61eccb31cf89af7b6ff0db2d992e447
spawn22 <todococo14@gmail.com>
2024-04-03 13:12:22 -0300

    eliminar 3er paso de instrucciones, correccion de ortografia y bug pequeño de estilo

--- src/components/molecules/MenuInstructionsBase/MenuInstructionsBase.jsx
+++ src/components/molecules/MenuInstructionsBase/MenuInstructionsBase.jsx
@@ -1,13 +1,13 @@
-import InstructionContainer from '../../atom/InstructionContainer/InstructionContainer';
-import LineText from '../../atom/LineText/LineText';
-import UploadMenuTitle from '../../atom/UploadMenuTitle/UploadMenuTitle';
-import InstructionButton from '../InstructionButton/InstructionButton';
-import MenuStep from '../MenuStep/MenuStep';
-import s from './MenuInstructionsBase.module.scss';
-import { useTranslation } from 'react-i18next';
+import InstructionContainer from "../../atom/InstructionContainer/InstructionContainer";
+import LineText from "../../atom/LineText/LineText";
+import UploadMenuTitle from "../../atom/UploadMenuTitle/UploadMenuTitle";
+import InstructionButton from "../InstructionButton/InstructionButton";
+import MenuStep from "../MenuStep/MenuStep";
+import s from "./MenuInstructionsBase.module.scss";
+import { useTranslation } from "react-i18next";
 
 export default function MenuInstructionsBase() {
-  const [t,i18n]=useTranslation("global");
+  const [t, i18n] = useTranslation("global");
   return (
     <InstructionContainer>
       <main className={s.mainContainer}>
@@ -18,13 +18,21 @@ export default function MenuInstructionsBase() {
             centered={true}
             bold={true}
           />
-          <MenuStep light={true} number={1} text={t("instructions.steps.step_1")} />
-          <MenuStep light={true}
+          <MenuStep
+            light={true}
+            number={1}
+            text={t("instructions.steps.step_1")}
+          />
+          <MenuStep
+            light={true}
             number={2}
             text={t("instructions.steps.step_2")}
           />
-          <MenuStep light={true} number={3} text={t("instructions.steps.step_3")} />
-          <MenuStep light={true} number={4} text={t("instructions.steps.step_4")} />
+          <MenuStep
+            light={true}
+            number={3}
+            text={t("instructions.steps.step_3")}
+          />
         </div>
         <InstructionButton
           helpText={t("instructions.button.i need help")}
