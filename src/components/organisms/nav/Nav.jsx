898d8fb9106ba8481448b38ff4239c002bc6052b
spawn22 <todococo14@gmail.com>
2024-04-10 11:10:15 -0300

    queda el menu habilitado y redirecciona a la pagina de octopussoft

--- src/components/organisms/nav/Nav.jsx
+++ src/components/organisms/nav/Nav.jsx
@@ -1,5 +1,5 @@
 import s from "./nav.module.scss";
-import { useState,useRef,useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import Bars from "../../molecules/nav/bars/Bars";
 import Open_closed from "../../molecules/nav/open_closed/Open_closed";
 import { Bars_3, Store, StoreClosed } from "../../atom/iconsHerocoins/icons";
@@ -7,7 +7,6 @@ import logoNav from "../../../assets/logo nav.svg";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-
 
 export default function Nav() {
   const [barsActive, setBarsActive] = useState(false);
@@ -25,7 +24,7 @@ export default function Nav() {
     userActive && setUserActive(false);
     setBarsActive(!barsActive);
   };
-  
+
   //cambia el fondo de los iconos
   const toggleUserBackground = async () => {
     barsActive && setBarsActive(false);
@@ -33,11 +32,17 @@ export default function Nav() {
   };
 
   const handleDocumentClick = (event) => {
-    if (barsContainerRef.current && !barsContainerRef.current.contains(event.target)) {
+    if (
+      barsContainerRef.current &&
+      !barsContainerRef.current.contains(event.target)
+    ) {
       setBarsActive(false);
     }
 
-    if (userContainerRef.current && !userContainerRef.current.contains(event.target)) {
+    if (
+      userContainerRef.current &&
+      !userContainerRef.current.contains(event.target)
+    ) {
       setTimeout(() => setUserActive(false), [250]);
     }
   };
@@ -49,22 +54,21 @@ export default function Nav() {
     };
   }, []);
 
-
   return (
-    <nav className={`${s.nav} ${disableNavbar ? s.disableNav : ""}`}>
+    <nav className={`${s.nav} `}>
       <div
         ref={barsContainerRef}
         className={`${s.iconContainer} ${
-        !barsActive ? s.customBackground : s.anotherCustomBackground
+          !barsActive ? s.customBackground : s.anotherCustomBackground
         }`}
         onClick={toggleBarsBackground}
       >
-        <Bars_3 heigth={36}/>
+        {disableNavbar ? null : <Bars_3 heigth={36} />}
       </div>
-        {barsActive ? <Bars onLinkClick={toggleBarsBackground}/> : null}
-        <Link to={"/dashboard"}>
-          <img src={logoNav} className={s.logo} />
-        </Link>
+      {barsActive ? <Bars onLinkClick={toggleBarsBackground} /> : null}
+      <Link to={"https://www.octopussoft.com.ar/"}>
+        <img src={logoNav} className={s.logo} />
+      </Link>
       <div
         className={`${s.iconContainer} ${
           !userActive ? s.customBackground : s.anotherCustomBackground
@@ -72,11 +76,23 @@ export default function Nav() {
         onClick={toggleUserBackground}
         ref={userContainerRef}
       >
-        {localValue ? <Store heigth={36} /> : <StoreClosed height={36} />}
+        {disableNavbar ? null : (
+          <>
+            {localValue ? (
+              <Store
+                heigth={36}
+                className={`${disableNavbar ? s.disableButton : ""}`}
+              />
+            ) : (
+              <StoreClosed
+                heigth={36}
+                className={`${disableNavbar ? s.disableButton : ""}`}
+              />
+            )}
+          </>
+        )}
       </div>
-      {userActive ? (
-        <Open_closed handlestate={toggleUserBackground}/>
-      ) : null}
+      {userActive ? <Open_closed handlestate={toggleUserBackground} /> : null}
     </nav>
   );
 }
