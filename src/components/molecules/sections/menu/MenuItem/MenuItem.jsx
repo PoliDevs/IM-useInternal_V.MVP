99165f1ff79f7e32099b437cdaa925453955ab99
spawn22 <todococo14@gmail.com>
2024-04-10 16:38:19 -0300

    Saco emoji y agrego category

--- src/components/molecules/sections/menu/MenuItem/MenuItem.jsx
+++ src/components/molecules/sections/menu/MenuItem/MenuItem.jsx
@@ -1,20 +1,18 @@
 import LineText from "../../../../atom/LineText/LineText";
-import SelectIcon from "../../../../atom/SelectIcon/SelectIcon";
 import s from "./MenuItem.module.scss";
 import { Button } from "semantic-ui-react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import Loading from "../../../../atom/loading/Loading";
 import { useTranslation } from "react-i18next";
-import { Eye,Eye_slash } from "../../../../atom/iconsHerocoins/icons";
-import { arrangedAlphabetically } from "../../../../../utils/functions";
+import { Eye, Eye_slash } from "../../../../atom/iconsHerocoins/icons";
 
 export default function MenuItem() {
-  const [t,i18n]=useTranslation("global");
-  const [menu,setMenu]=useState(false);
+  const [t, i18n] = useTranslation("global");
+  const [menu, setMenu] = useState(false);
 
-  const comerceId=useSelector(state=>state.user_internal.comerceId);
+  const comerceId = useSelector((state) => state.user_internal.comerceId);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,10 +24,10 @@ export default function MenuItem() {
       }
     };
     fetchData();
-  }, []);
+  }, [comerceId]);
 
   const handleClickEyes = async (index, id) => {
-    console.log(index,id)
+    console.log(index, id);
     try {
       const updatedMenu = [...menu]; // Clonar el estado del menú actual
       const updatedItem = { ...updatedMenu[index] }; // Clonar el elemento actual
@@ -47,53 +45,70 @@ export default function MenuItem() {
     }
   };
 
-
   //const sortedMenu = menu && menu.length&& menu.sort((a, b) => (a.active === b.active ? 0 : a.active ? -1 : 1));
 
-  const menuToLowerCase=menu && menu.length && menu.map(cur=>({...cur,name:cur.name.toLowerCase()}));
-  console.log(menuToLowerCase)
-  const sortedMenu = menu && menu.length && menu.sort((a, b) => {
-    // Ordenar por estado activo (activos primero)
-    const sortByActive = a.active === b.active ? 0 : a.active ? -1 : 1;
-  
-    // Si tienen el mismo estado, ordenar alfabéticamente por nombre
-    const sortByAlphabetical = a.name.localeCompare(b.name);
-  
-    // Aplicar ambos criterios de ordenación
-    return sortByActive !== 0 ? sortByActive : sortByAlphabetical;
-  });
-  
+  const menuToLowerCase =
+    menu &&
+    menu.length &&
+    menu.map((cur) => ({ ...cur, name: cur.name.toLowerCase() }));
+  console.log(menuToLowerCase);
+  const sortedMenu =
+    menu &&
+    menu.length &&
+    menu.sort((a, b) => {
+      // Ordenar por estado activo (activos primero)
+      const sortByActive = a.active === b.active ? 0 : a.active ? -1 : 1;
+
+      // Si tienen el mismo estado, ordenar alfabéticamente por nombre
+      const sortByAlphabetical = a.name.localeCompare(b.name);
+
+      // Aplicar ambos criterios de ordenación
+      return sortByActive !== 0 ? sortByActive : sortByAlphabetical;
+    });
+  console.log(menu);
 
   return (
     <div className={s.menuItemContainer}>
-      <Button primary size="huge" disabled >{t("menu.menu item.add product")}</Button>
+      <Button primary size="huge" disabled>
+        {t("menu.menu item.add product")}
+      </Button>
       <div>
         <div className={s.content_menu_table}>
           <section className={s.menu_opciones}>
-            <LineText text={t("menu.menu item.emoji")} secundary={true}  />
-            <LineText text={t("menu.menu item.product")} secundary={true}  />
-            <LineText text={t("menu.menu item.price")} secundary={true}  />
+            {/* <LineText text={t("menu.menu item.emoji")} secundary={true} /> */}
+            <LineText text={t("menu.menu item.category")} secundary={true} />
+            <LineText text={t("menu.menu item.product")} secundary={true} />
+            <LineText text={t("menu.menu item.price")} secundary={true} />
             <LineText text={t("menu.menu item.on/off")} secundary={true} />
           </section>
           <div className={s.menu_table}>
-            {!sortedMenu?<Loading/>:sortedMenu.map((item, index) => (
-              <div
-                key={index}
-                className={`${s.line_menu} ${
-                  !item.active ? s.inactiveLine : ""
-                }`}
-              >
-                <SelectIcon className={s.emoji} icon={item.photo} />
-                <LineText text={item.name} disabled={!item.active} />
-                <LineText text={`$${item.cost}`} disabled={!item.active} />
-                <div onClick={() => handleClickEyes(index,item.id)}>
-                  {item.active?<Eye heigth={24} ></Eye>:<Eye_slash heigth={24}/>}
+            {!sortedMenu ? (
+              <Loading />
+            ) : (
+              sortedMenu.map((item, index) => (
+                <div
+                  key={index}
+                  className={`${s.line_menu} ${
+                    !item.active ? s.inactiveLine : ""
+                  }`}
+                >
+                  <LineText text={item.category} disabled={!item.active} />
+                  {/* <SelectIcon className={s.emoji} icon={item.photo} /> */}
+                  <LineText text={item.name} disabled={!item.active} />
+                  <LineText text={`$${item.cost}`} disabled={!item.active} />
+                  <div onClick={() => handleClickEyes(index, item.id)}>
+                    {item.active ? (
+                      <Eye heigth={24}></Eye>
+                    ) : (
+                      <Eye_slash heigth={24} />
+                    )}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
