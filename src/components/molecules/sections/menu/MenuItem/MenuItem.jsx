b933a177bdc694d8787e51858efec2490ee5f041
Nico <medinanico93@gmail.com>
2024-04-17 09:28:20 -0300

    Cambios en el orden y apagado de los productos

--- src/components/molecules/sections/menu/MenuItem/MenuItem.jsx
+++ src/components/molecules/sections/menu/MenuItem/MenuItem.jsx
@@ -57,13 +57,13 @@ export default function MenuItem() {
     menu.length &&
     menu.sort((a, b) => {
       // Ordenar por estado activo (activos primero)
-      const sortByActive = a.active === b.active ? 0 : a.active ? -1 : 1;
+     /* const sortByActive = a.active === b.active ? 0 : a.active ? -1 : 1;
 
       // Si tienen el mismo estado, ordenar alfabéticamente por nombre
       const sortByAlphabetical = a.name.localeCompare(b.name);
 
       // Aplicar ambos criterios de ordenación
-      return sortByActive !== 0 ? sortByActive : sortByAlphabetical;
+      return sortByActive !== 0 ? sortByActive : sortByAlphabetical;*/
     });
   console.log(menu);
 
