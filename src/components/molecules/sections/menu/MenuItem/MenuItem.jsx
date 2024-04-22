44075887deefa3db9adcfcba36f7b5ced2ae0cc8
spawn22 <todococo14@gmail.com>
2024-04-22 10:12:43 -0300

    agrego textos para carteles de

--- src/components/molecules/sections/menu/MenuItem/MenuItem.jsx
+++ src/components/molecules/sections/menu/MenuItem/MenuItem.jsx
@@ -14,7 +14,6 @@ export default function MenuItem() {
   const [t, i18n] = useTranslation("global");
   const [menu, setMenu] = useState(false);
 
-  // const [activeEyeId, setActiveEye] = useState(null);
   const comerceId = useSelector((state) => state.user_internal.comerceId);
   const menuChanges = useSelector((state) => state.changes);
   const dispatch = useDispatch();
@@ -38,17 +37,7 @@ export default function MenuItem() {
     setMenu(updatedMenu);
 
     dispatch(setMenuChanges({ index, id, active: !menu[index].active }));
-    // clearTimeout(debounceTimeout.current);
-
-    // debounceTimeout.current = setTimeout(() => {
-    //   applyMenuChanges();
-    // }, 1000);
-    // clearTimeout(debounceTimeout.current);
   };
-
-  // const debounceTimeout = useRef(null);
-
-  //const sortedMenu = menu && menu.length&& menu.sort((a, b) => (a.active === b.active ? 0 : a.active ? -1 : 1));
 
   const menuWithCapitalizedNames =
     menu &&
@@ -78,7 +67,7 @@ export default function MenuItem() {
           <section className={s.menu_opciones}>
             {/* <LineText text={t("menu.menu item.emoji")} secundary={true} /> */}
             <LineText text={t("menu.menu item.category")} secundary={true} />
-            <LineText text={t("menu.menu item.menu")} secundary={true} />
+            <LineText text={t("menu.menu item.product")} secundary={true} />
             <LineText text={t("menu.menu item.price")} secundary={true} />
             <LineText text={t("menu.menu item.on/off")} secundary={true} />
           </section>
@@ -97,9 +86,9 @@ export default function MenuItem() {
                   {/* <LineText className={s.emoji} icon={item.photo} /> */}
                   <LineText text={item.name} disabled={!item.active} />
                   <LineText text={`$${item.cost}`} disabled={!item.active} />
-                  <div onClick={() => handleClickEyes(index, item.id)}>
+                  <div onClick={() => handleClickEyes(index, item.id)} className={s.imageContainer}>
                     {item.active ? (
-                      <Eye heigth={24}></Eye>
+                      <Eye heigth={24}></Eye> 
                     ) : (
                       <Eye_slash heigth={24} />
                     )}
