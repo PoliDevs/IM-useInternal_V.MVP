9ae30bceabd6732e56ba134421c80a60137f99a5
Nico <medinanico93@gmail.com>
2024-05-06 17:16:23 -0300

    Arreglo de icons y sign_in

--- src/components/atom/iconsHerocoins/icons.jsx
+++ src/components/atom/iconsHerocoins/icons.jsx
@@ -74,9 +74,8 @@ export function Logout({ height }) {
      viewBox="0 0 512 512"
      fill="none"
      stroke="currentColor" 
-     stroke-linecap="round" 
-     stroke-linejoin="round" 
-     stroke-width="32"
+     strokeLinejoin="round" 
+     strokeWidth="32"
      height={height || "30"}
     >
       <path d="M304 336v40a40 40 0 01-40 40H104a40 40 0 01-40-40V136a40 40 0 0140-40h152c22.09 0 48 17.91 48 40v40M368 336l80-80-80-80M176 256h256" /></svg>
